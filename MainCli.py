eeef3e38d8a94f69303202ebbbd5625dac8c25d6
Murphy <54226881+murphys7017@users.noreply.github.com>
2025-02-22 05:29:59 +0800

    通过回车发送方式释放其他监听

--- MainCli.py
+++ MainCli.py
@@ -36,6 +36,39 @@ class ScriptManagerPressThread(QThread):
                 # 使用信号将处理结果返回到主线程
                 self.script_manager_result_signal.emit(res)
                 
+class myTextEdit(QTextEdit): #继承 原本组件
+    def __init__(self,parent):
+        QTextEdit.__init__(self)
+        self.parent = parent
+
+    def keyPressEvent(self, event):
+        """处理回车事件"""
+        if event.key() not in [Qt.Key_Return, Qt.Key_Enter]:
+            super().keyPressEvent(event)
+            return
+        edit_line = self.parent.lines[len(self.parent.text_lines)].toPlainText()
+        user_input = edit_line.split(self.parent.split_char)[-1].strip()  # 获取用户输入
+        # 获取当前输入的文本
+        logger.info(f"User input: {user_input}")
+        if user_input.lower() == "exit":  # 输入 "exit" 退出程序
+            self.parent.close()
+            return
+        
+        self.parent.update_text_show(edit_line,need_stream_show=False)
+        
+        message = {'category':'message','content':user_input}
+        if user_input.startswith('/'):
+            message['category'] = 'command'
+        # # 显示响应
+        
+        self.parent.script_manager_thread = ScriptManagerPressThread(self.parent.scriptManager,message)
+        self.parent.script_manager_thread.script_manager_result_signal.connect(self.parent.on_script_manager_message)  # 连接信号到槽
+        self.parent.script_manager_thread.start()  # 启动线程
+        # 开始显示 "..."（类似于loading效果）
+        self.parent.dots_count = 0  # 重置计数
+        self.parent.loading_timer.start(500)  # 每隔500ms更新一次文本，模拟加载效果
+        self.parent.lines[len(self.parent.text_lines)].setReadOnly(False)  # 文本可编辑
+
 class CLIWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -59,7 +92,7 @@ class CLIWindow(QMainWindow):
         #background-color: transparent; 
         #   color: #619B5F; 
         for _ in range(7):
-            tempLine = QTextEdit(self)
+            tempLine = myTextEdit(self)
             tempLine.setStyleSheet("""QTextEdit {
                 background-color: transparent; 
                 color: #619B5F; 
@@ -83,13 +116,11 @@ class CLIWindow(QMainWindow):
         self.text_lines = self.line_history[-6:] 
         for line, text in zip(self.lines, self.text_lines):
             line.setText(text)  # 更新文本行
-        self.lines[len(self.line_history)].setReadOnly(False)  # 文本可编辑
-        self.lines[len(self.line_history)].setText(self.placeholder_text)  # >文本
-        self.lines[len(self.line_history)].setFocus()
-        cursor = self.lines[len(self.line_history)].textCursor()
-        cursor.movePosition(QTextCursor.End) # 还可以有别的位置
-        self.lines[len(self.line_history)].setTextCursor(cursor)
-        self.on_text_changed(len(self.line_history))
+        index = len(self.line_history)
+        self.lines[index].setReadOnly(False)  # 文本可编辑
+        self.lines[index].setText(self.placeholder_text)  # >文本
+        self.reset_cursor_to_end(index)
+        self.on_text_changed(index)
 
         # 布局设置
         layout = QVBoxLayout()
@@ -141,53 +172,18 @@ class CLIWindow(QMainWindow):
             self.lines[len(self.text_lines)].setText(self.placeholder_text)  # >文本
             self.lines[len(self.text_lines)].setReadOnly(True)  # 文本可编辑
         self.update_text_show(f"{self.bot_name}: {response}")
+    def reset_cursor_to_end(self,line_index):
+        self.lines[line_index].setFocus()
+        cursor = self.lines[line_index].textCursor()
+        cursor.movePosition(QTextCursor.End) # 还可以有别的位置
+        self.lines[line_index].setTextCursor(cursor)
     def ctrl_q_pressed(self):
         """Ctrl+Q 被按下时触发的操作"""
         if len(self.line_history) > 7 : 
-            self.lines[6].setFocus()
-            cursor = self.lines[len(self.line_history)].textCursor()
-            cursor.movePosition(QTextCursor.End) # 还可以有别的位置
-            self.lines[len(self.line_history)].setTextCursor(cursor)
+            self.reset_cursor_to_end(6)
         else:
+            self.reset_cursor_to_end(len(self.text_lines))
 
-            self.lines[len(self.text_lines)].setFocus()
-            cursor = self.lines[len(self.line_history)].textCursor()
-            cursor.movePosition(QTextCursor.End) # 还可以有别的位置
-            self.lines[len(self.line_history)].setTextCursor(cursor)
-
-
-    def keyPressEvent(self, event):
-        """处理回车事件"""
-        logger.info(event)
-        if event.key() == Qt.Key_Enter:
-            print(111)
-        if event.key() == Qt.Key_Return or event.key() == Qt.Key_Enter:
-            pass
-        else:
-            super().keyPressEvent(event)
-        edit_line = self.lines[len(self.text_lines)].toPlainText()
-        user_input = edit_line.split(self.split_char)[-1].strip()  # 获取用户输入
-        # 获取当前输入的文本
-        logger.info(f"User input: {user_input}")
-        if user_input.lower() == "exit":  # 输入 "exit" 退出程序
-            self.close()
-            return
-        
-        self.update_text_show(edit_line,need_stream_show=False)
-        
-        message = {'category':'message','content':user_input}
-        if user_input.startswith('/'):
-            message['category'] = 'command'
-        # # 显示响应
-        
-        self.script_manager_thread = ScriptManagerPressThread(self.scriptManager,message)
-        self.script_manager_thread.script_manager_result_signal.connect(self.on_script_manager_message)  # 连接信号到槽
-        self.script_manager_thread.start()  # 启动线程
-        # 开始显示 "..."（类似于loading效果）
-        self.dots_count = 0  # 重置计数
-        self.loading_timer.start(500)  # 每隔500ms更新一次文本，模拟加载效果
-        self.lines[len(self.text_lines)].setReadOnly(False)  # 文本可编辑
-        
     def update_loading_text(self):
         """更新加载文本"""
         # 根据计数更新文本
@@ -199,33 +195,28 @@ class CLIWindow(QMainWindow):
 
 
     def update_text_show(self,message, need_stream_show=True):
-            message = message.strip() 
-            self.line_history.append(message)  # 记录用户输入
-            self.text_lines = self.line_history[-6:]
+        message = message.strip() 
+        self.line_history.append(message)  # 记录用户输入
+        self.text_lines = self.line_history[-6:]
 
-            if len(self.line_history) > 7 : 
-                for line, text in zip(self.lines, self.text_lines):
-                    line.setText(text)  # 更新文本行
-                self.lines[6].setReadOnly(False)  # 文本可编辑
-                # self.lines[6].setPlaceholderText()  # 显示输入提示符
-                self.lines[6].setText(self.placeholder_text)  # >文本
-                self.lines[6].setFocus()
-                cursor = self.lines[6].textCursor()
-                cursor.movePosition(QTextCursor.End) # 还可以有别的位置
-                self.lines[6].setTextCursor(cursor)
-            else:
-                for line, text in zip(self.lines, self.text_lines):
-                    line.setText(text)  # 更新文本行
-                self.lines[len(self.text_lines)].setReadOnly(False)  # 文本可编辑
-                # self.lines[len(self.text_lines)].setPlaceholderText("> ")  # 显示输入提示符
-                self.lines[len(self.text_lines)].setText(self.placeholder_text)  # >文本
-                self.lines[len(self.text_lines)].setFocus()
-                cursor = self.lines[len(self.line_history)].textCursor()
-                cursor.movePosition(QTextCursor.End) # 还可以有别的位置
-                self.lines[len(self.line_history)].setTextCursor(cursor)
+        if len(self.line_history) > 7: 
+            for line, text in zip(self.lines, self.text_lines):
+                line.setText(text)  # 更新文本行
+            self.lines[6].setReadOnly(False)  # 文本可编辑
+            # self.lines[6].setPlaceholderText()  # 显示输入提示符
+            self.lines[6].setText(self.placeholder_text)  # >文本
+            self.reset_cursor_to_end(6)
+        else:
+            for line, text in zip(self.lines, self.text_lines):
+                line.setText(text)  # 更新文本行
+            index = len(self.text_lines)
+            self.lines[index].setReadOnly(False)  # 文本可编辑
+            # self.lines[len(self.text_lines)].setPlaceholderText("> ")  # 显示输入提示符
+            self.lines[index].setText(self.placeholder_text)  # >文本
+            self.reset_cursor_to_end(index)
             # # 显示响应
-            if need_stream_show:
-                self.display_response(message)
+        if need_stream_show:
+            self.display_response(message)
     def on_text_changed(self,target_line,textExit=None):
             """文本变化时自动调整大小"""
             if textExit:
